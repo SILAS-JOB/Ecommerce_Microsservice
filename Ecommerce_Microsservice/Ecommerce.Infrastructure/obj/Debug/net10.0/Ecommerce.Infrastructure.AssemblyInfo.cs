153eade39127f839cce452ba29e5c6936076a630
SILAS-JOB <tiilas777@gmail.com>
2025-12-23 21:15:52 -0300

    hotfix

--- Ecommerce_Microsservice/Ecommerce.Infrastructure/obj/Debug/net10.0/Ecommerce.Infrastructure.AssemblyInfo.cs
+++ Ecommerce_Microsservice/Ecommerce.Infrastructure/obj/Debug/net10.0/Ecommerce.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8135128df7a6c6c815a6380610903d1e69fe532")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7450bb903f7f0cba0fe91f81d22d6269a4eae945")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
