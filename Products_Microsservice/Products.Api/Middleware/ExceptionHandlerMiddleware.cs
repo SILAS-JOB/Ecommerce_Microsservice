736ca5d132b5b33289f4afacba5d40ad3e370a10
SILAS-JOB <tiilas777@gmail.com>
2025-12-23 21:15:42 -0300

    feat: Exception middleware

---
+++ Products_Microsservice/Products.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Products.Api.Middleware;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
+    public ExceptionHandlerMiddleware(RequestDelegate next, ILogger logger)
+    {
+        _next = next;
+        _logger = logger;
+    }       
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch(Exception ex)
+        {
+            if (ex.InnerException != null)
+            {
+                _logger.LogError("{ExceptionType}{ExceptionMessage}",
+                ex.InnerException.GetType().ToString(),
+                ex.InnerException.Message);
+            }
+            else
+            {
+                _logger.LogError("{ExceptionType}{ExceptionMessage}",
+                ex.GetType().ToString(), ex.Message);
+            }
+
+            httpContext.Response.StatusCode = 500;
+            await httpContext.Response.WriteAsJsonAsync(new { Message = ex.Message, Type = ex.GetType().ToString() });
+        }
+    }
+}
+
+public static class ExceptionHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+    }
+}
--- Products_Microsservice/Products.Api/Program.cs
+++ Products_Microsservice/Products.Api/Program.cs
@@ -1,5 +1,25 @@
+using BusinessLogicLayer;
+using DataAcessLayer;
+using FluentValidation.AspNetCore;
+using Products.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataAcessLayer(builder.Configuration);
+builder.Services.AddBusinessLogicLayer();
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+
 var app = builder.Build();
 
+app.UseExceptionHandlerMiddleware();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
