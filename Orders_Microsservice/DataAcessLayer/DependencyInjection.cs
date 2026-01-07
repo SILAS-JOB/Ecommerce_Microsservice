65adf29c4e3182763edfb76b1de58207b1305a1d
SILAS-JOB <tiilas777@gmail.com>
2026-01-07 01:02:23 +0000

    feat: DI mongoDB

--- Orders_Microsservice/DataAcessLayer/DependencyInjection.cs
+++ Orders_Microsservice/DataAcessLayer/DependencyInjection.cs
@@ -5,8 +5,24 @@ namespace DataAcessLayer;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDataAcessLayer(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+  {
+    string connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+    string connectionString = connectionStringTemplate
+      .Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST"))
+      .Replace("$MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
+
+    services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+    services.AddScoped<IMongoDatabase>(provider =>
     {
-        return services;
-    }
+      IMongoClient client = provider.GetRequiredService<IMongoClient>();
+      return client.GetDatabase("OrdersDatabase");
+    });
+
+    services.AddScoped<IOrdersRepository, OrdersRepository>();
+
+
+    return services;
+  }
 }
