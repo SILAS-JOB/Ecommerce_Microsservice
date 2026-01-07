87c2fd48b9ac884cf83eacc3a3595065ed642620
SILAS-JOB <tiilas777@gmail.com>
2026-01-07 00:58:28 +0000

    feat: Orders Request layer

---
+++ Orders_Microsservice/BusinessLogicLayer/DTO/OrderItemResponse.cs
@@ -0,0 +1,8 @@
+namespace BusinessLogicLayer.DTO;
+
+public record OrderItemResponse(Guid ProductID, decimal UnitPrice, int Quantity, decimal TotalPrice)
+{
+  public OrderItemResponse() : this(default, default, default, default)
+  {
+  }
+}
---
+++ Orders_Microsservice/BusinessLogicLayer/DTO/OrderUpdateRequest.cs
@@ -0,0 +1,8 @@
+namespace BusinessLogicLayer.DTO;
+
+public record OrderUpdateRequest(Guid OrderID, Guid UserID, DateTime OrderDate, List<OrderItemUpdateRequest> OrderItems)
+{
+  public OrderUpdateRequest() : this(default, default, default, default)
+  {
+  }
+}
